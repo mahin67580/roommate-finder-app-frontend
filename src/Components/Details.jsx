b91f0bb83036d7a88a1bf779e31be2362894cd80
mahin67580 <mahin67580@gmail.com>
2025-05-22 17:06:54 +0600

    challenge one done

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,18 +1,65 @@
-import React from 'react';
+import React, { use, useState } from 'react';
 import { useLoaderData } from 'react-router';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import Swal from 'sweetalert2';
+import { AuthContext } from '../Provider/AuthContext';
 
 const Details = () => {
     const post = useLoaderData()
-    console.log(post);
-    const { _id, title, rent, location, availability, contact, description, lifestyle, roomType,email } = post;
+    const { user } = use(AuthContext)  
+    const [count, setCount] = useState(post?.likes || 0);
+    const { _id, title, rent, location, availability, contact, description, lifestyle, roomType, email } = post;
+
+
+    const isOwner = user?.email === email;
+
+    const handleLike = (id) => {
+
+        if (isOwner) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "You can't like your own post!",
+            });
+            return;
+        }
+
+        const newCount = count + 1;
+        setCount(newCount);
+
+
+        fetch(`http://localhost:3000/roommates/${id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ likes: newCount }),
+        }).then(res => res.json()).then(data => {
+            console.log("data from serverDB", data);
+            if (data.acknowledged) {
+
+
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "You Liked it",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+
+
+
+            }
+        })
+    }
+
+
     return (
         <div>
             <Navbar></Navbar>
             <div className="bg-white m-6 text-center  p-6 rounded-xl shadow-md border hover:shadow-lg transition duration-300 w-11/12 mx-auto">
-                <h2 className="text-xl font-bold mb-2 text-gray-800">{title}</h2>
-                <p className="text-gray-600"><strong>Location:</strong> {location}</p>
+                <p className='text-green-500 font-bold mb-4'><span className='text-2xl'>{count}</span> people interested in this person</p>
+                <h2 className="text-xl font-bold mb-4 text-gray-800">{title}</h2>
+                <p className="text-gray-600 mb-4"><strong>Location:</strong> {location}</p>
                 <p className="text-gray-600 mb-4"><strong>Rent:</strong> {rent}</p>
                 <p className="text-gray-600 mb-4"><strong>availability:</strong> {availability}</p>
                 <p className="text-gray-600 mb-4"><strong>contact:</strong> {contact}</p>
@@ -20,7 +67,7 @@ const Details = () => {
                 <p className="text-gray-600 mb-4"><strong>lifestyle:</strong> {lifestyle}</p>
                 <p className="text-gray-600 mb-4"><strong>roomType:</strong> {roomType}</p>
                 <p className="text-gray-600 mb-4"><strong>email:</strong> {email}</p>
-                <button className="inline-block mt-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">Like</button>
+                <button onClick={() => handleLike(_id)} className="inline-block mt-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">Like</button>
             </div>
             <Footer></Footer>
         </div>
